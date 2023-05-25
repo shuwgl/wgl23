9ddb501a2fa8ef5e38eb5dce172fc02378801935
Stephane Huart <stephane.huart@interface3.be>
2023-05-25 13:25:41 +0200

    Add state

--- render2d.js
+++ render2d.js
@@ -44,6 +44,8 @@ var createCharac = function(name, pv, tex){
 var createRenderEngine = function (canvasTarget) {
     var self = {};
     var posInit =100;
+    var state = "right"
+    var speed =1
 
     var init = function () {
         var canvas = document.querySelector(canvasTarget)
@@ -56,21 +58,45 @@ var createRenderEngine = function (canvasTarget) {
 
         var onKeydown = function (event) {
             console.log(event);
+
             if (event.key =="d") {
-                ctx.clearRect(0,0,canvas.width, canvas.height)
-                posInit = posInit+1 
-                ctx.fillStyle ="blue"
-                ctx.fillRect(posInit,50,50,50);
+                state ="right"
                 
             }
             if (event.key =="q") {
-                ctx.clearRect(0,0,canvas.width, canvas.height)
-                posInit = posInit-1 
-                ctx.fillStyle ="green"
-                ctx.fillRect(posInit,50,-50,50);
+                state ="left"
             }
             //A ajouter ici
         }
+
+        var render=function () {
+            ctx.clearRect(0,0,canvas.width, canvas.height)
+            //posInit = posInit+1 
+            ctx.fillStyle ="green"
+            ctx.fillRect(posInit,50,-50,50);
+        }
+
+        var process = function() {
+            if (state == "right") {
+                posInit = posInit+speed
+            }
+            if (state == "left") {
+                posInit = posInit-speed
+            }
+            // Définir ici une mise à jour de "poseInit" selon que le state du cube soit
+            //"right" ou "left"
+            // definir une variable state
+            //definir un process qui selon le state change posInit vers la droite ou la gauche
+            //lier les changement de state a la touche "d" ou "q"
+        }
+
+        var renderFrame = function () {
+            process()//check if we need to change the cube position
+            render()
+            console.log("hello");
+            window.requestAnimationFrame(renderFrame)
+        }
+        renderFrame()
 
         document.addEventListener("keydown", onKeydown)
     }
